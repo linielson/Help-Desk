0e94156c4feccc56d1a9ff76cfaf4bc50fd7df4c
Linielson Rosa <linielsonrosa@gmail.com>
2012-05-04 15:28:16 -0300

    Gravatar

--- app/helpers/usuarios_helper.rb
+++ app/helpers/usuarios_helper.rb
@@ -1,2 +1,8 @@
 module UsuariosHelper
+
+  def gravatar_image_tag(email)
+    email = Digest::MD5.hexdigest(email)
+    image_tag "http://www.gravatar.com/avatar/#{email}", alt: "Imagem Gravatar", size: "48x48"
+  end
+
 end
