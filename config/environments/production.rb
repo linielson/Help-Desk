5368d02e20419cf499369f0bdf97f1c25aff8924
Linielson Rosa <linielsonrosa@gmail.com>
2012-05-04 10:36:57 -0300

    Teste E-mail e Hora

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,11 @@ Helpdesk::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.default_url_options = {host: 'localhost:3000'}
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.time_zone = 'Brasilia'
+  config.active_record.default_timezone = :local
 end
